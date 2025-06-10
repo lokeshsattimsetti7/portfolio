6653c2271e4a4e38a9d9c4e21c95bfcdd235b2fe
lokeshsattimsetti7 <lokeshsattimsetti@gmail.com>
2025-06-11 02:30:15 +0530

    Update Project.jsx

--- examplee/src/Components/Projects/Project.jsx
+++ examplee/src/Components/Projects/Project.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./Project.module.css";
 import One from "./E.png";
-
+import Two from "./spotify.png";
+import Three from "./blog.png";
 const Project = () => {
   return (
     <div className={styles.MainSection}>
@@ -39,6 +40,73 @@ const Project = () => {
 
         <div className={styles.RightSide}>
           <img src={One} alt="one" />
+        </div>
+      </div>
+      <div className={styles.Block}>
+        <div className={styles.LeftSide}>
+          <h1> Spotify-Frontend</h1>
+
+          <p1>
+            This project is a modern, responsive web application that replicates
+            the user interface of the Spotify music platform. Built using React
+            JS and Tailwind CSS, it showcases a sleek, interactive design that
+            mirrors the core visual elements of Spotify, offering users an
+            immersive UI experience. While the application focuses solely on the
+            frontend, it effectively demonstrates the layout and design
+            principles of a high-quality music streaming service.
+          </p1>
+          <div className={styles.Button}>
+            {" "}
+            <a
+              href=" https://spotify-clone-git-main-lokeshsattimsetti7s-projects.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className={styles.glow}>Website link</button>
+            </a>
+            <a
+              href="https://github.com/lokeshsattimsetti7/SpotifyClone.git"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className={styles.glow}>Github</button>
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.RightSide}>
+          <img src={Two} alt="one" />
+        </div>
+      </div>
+      <div className={styles.Block}>
+        <div className={styles.LeftSide}>
+          <h1> Blogy-Website</h1>
+
+          <p1>
+            Blogy is a fully responsive blog website, Responsive for all
+            devices, build using HTML, CSS, and JavaScript.
+          </p1>
+          <div className={styles.Button}>
+            {" "}
+            <a
+              href="http://127.0.0.1:5501/blogy-master/index.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className={styles.glow}>Website link</button>
+            </a>
+            <a
+              href="https://github.com/lokeshsattimsetti7/Blog.git"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className={styles.glow}>Github</button>
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.RightSide}>
+          <img src={Three} alt="one" />
         </div>
       </div>
     </div>
